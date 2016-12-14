f244a0d3e371131d43f5557d6652b62ed7ec8232
boyd <8184160900.cell@gmail.com>
2016-12-14 10:32:58 +0000

    pendo

---
+++ pendo/credentials.py
@@ -0,0 +1,26 @@
+__author__ = 'dmitry_boyko'
+
+class GooglePage():
+
+    def open(self):
+        return {
+            "google_url": "https://www.google.com/",
+            "yahoo_url":  "https://www.yahoo.com/"
+            }
+
+    def search(self):
+        return {
+            "h_sack": "hacky sack",                 # string
+            "search_fld": "lst-ib",                 # by ID
+            "search_icon": "_fZl",                  # by  ID
+            "shopping": "a.q.qs"                     # by CSS
+            }
+
+    def save_to_shop_list(self):
+        return {
+            "g4": "//div[4]/div/h3/a",              # by xpath:position
+            "addToList": "//div[4]/div[2]/div",     # by xpath
+            "save": "div.gko-a-lbl",                # by CSS
+            "note": "Please buy me"                 # string
+            }
+
---
+++ pendo/test.py
@@ -0,0 +1,82 @@
+__author__ = 'dmitry_boyko'
+
+"""
+1. open
+2. search(search_term)
+3. save_to_shop_list(item_number, note)
+
+google_page = GooglePage(selenium_driver)
+google_page.open()
+google_page.search("hacky sack")
+google_page.shopping_page("hacky sacks")
+google_page.save_to_shop_list(4, "Please buy me")
+"""
+
+import sys
+from selenium                           import webdriver
+from selenium.webdriver.common.by       import By
+from credentials                        import GooglePage
+from selenium.webdriver.common.keys     import Keys
+
+def driver():
+    selenium_driver = webdriver.Chrome()
+    #selenium_driver = webdriver.Firefox()
+    selenium_driver.set_page_load_timeout(30)
+    selenium_driver.implicitly_wait(30)
+    return selenium_driver
+
+
+def main():
+    d = driver()
+
+    browse = browse_and_buy()
+    browse.open_browser(d)
+    browse.search_item(d)
+    browse.shopping_page(d)
+    browse.save_to_shop_list(d)
+    d.quit()
+
+
+class browse_and_buy():
+
+    def open_browser(self, selenium_driver):
+        gp = GooglePage()
+        selenium_driver.get(gp.open()["google_url"])
+
+    def search_item(self, selenium_driver):
+        gp = GooglePage()
+        try:
+            selenium_driver.find_element(By.ID, gp.search()["search_fld"]).send_keys(gp.search()["h_sack"])
+            selenium_driver.find_element(By.ID, gp.search()["search_icon"]).send_keys(Keys.RETURN)
+        except Exception, err:
+            sys.stderr.write('ERROR: %sn' % str(err))
+            return 1
+
+    def shopping_page(self, selenium_driver):
+        gp = GooglePage()
+        try:
+            selenium_driver.find_element(By.CSS_SELECTOR, gp.search()["shopping"]).click()
+        except Exception, err:
+            sys.stderr.write('ERROR: %sn' % str(err))
+            return 1
+
+    def save_to_shop_list(self, selenium_driver):
+        gp = GooglePage()
+        try:
+            elements = selenium_driver.find_elements(By.XPATH, gp.save_to_shop_list()["g4"])
+            for element4th in elements:
+                if element4th.is_displayed():
+                    if element4th.is_enabled():
+                        selenium_driver.find_element(By.XPATH, gp.save_to_shop_list()["g4"]).click()
+                        addToList = selenium_driver.find_elements(By.XPATH, gp.save_to_shop_list()["addToList"])
+                        for element2lib in addToList:
+                            if element2lib.is_displayed():
+                                if element2lib.is_enabled():
+                                    selenium_driver.find_element(By.XPATH, gp.save_to_shop_list()["addToList"]).click()
+            print (gp.save_to_shop_list()["note"])
+        except Exception, err:
+            sys.stderr.write('ERROR: %sn' % str(err))
+            return 1
+
+if __name__ == "__main__":
+    main()
